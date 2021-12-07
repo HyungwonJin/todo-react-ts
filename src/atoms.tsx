1ba6d9ad4306e658c120c4c73bf2d31cfc4d22d4
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-12-07 20:36:47 +0900

    chore: enum으로 category를 정리

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,15 +1,21 @@
 import { atom, selector } from "recoil";
 
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   // 사용자가 현재 지정한 category
   key: "category",
-  default: "TO_DO",
+  default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDo[]>({
